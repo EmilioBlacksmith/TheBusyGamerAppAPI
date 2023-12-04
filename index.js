c065a66d7f9e8bf30e1630afc46c7d05dd5112d4
Emilio Blacksmith <emilioblacksmithlush@gmail.com>
2023-12-03 19:38:36 -0600

    search query done

---
+++ index.js
@@ -0,0 +1,41 @@
+const app = require("express")();
+const axios = require("axios");
+const PORT = 8080;
+
+const hltb = require("howlongtobeat");
+let hltbService = new hltb.HowLongToBeatService();
+
+app.get("/test", (req, res) => {
+  res.status(200).send({
+    message: "nigga",
+    size: "medium",
+  });
+});
+
+app.get("/search", async (req, res) => {
+  try {
+    const searchQuery = req.query.q;
+
+    if (!searchQuery) {
+      return res.status(400).json({ error: "Search query parameter (q)" });
+    }
+
+    const result = await hltbService.search(`${searchQuery}`);
+
+    if (result.length <= 0) {
+      return res.status(300).json({ error: "Search query not found" });
+    }
+
+    return res.status(200).json(result);
+  } catch (error) {
+    console.error("Error:", error.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+app.listen(PORT, () =>
+  console.log(
+    `> Server is running on http://localhost:${PORT}\n`,
+    "- CTRL + C to close server..."
+  )
+);
