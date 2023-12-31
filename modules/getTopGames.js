14462ce465bdee13d0370d14102ac527aae9dd1f
Emilio Blacksmith <emilioblacksmithlush@gmail.com>
2023-12-31 17:17:54 -0600

    Updated args and executable path for Puppeteer launch

--- modules/getTopGames.js
+++ modules/getTopGames.js
@@ -1,6 +1,7 @@
 // WEB SCRAPPER made combined with the HowLongToBeat API by Christian Katzorke, made by Emilio Blacksmith
 // https://github.com/ckatzorke/howlongtobeat
 const puppeteer = require("puppeteer");
+require("dotenv").config();
 const cheerio = require("cheerio");
 const hltb = require("howlongtobeat");
 const fs = require("fs").promises;
@@ -9,58 +10,69 @@ const hltbService = new hltb.HowLongToBeatService();
 const BASE_URL = "https://howlongtobeat.com/stats";
 
 async function getTopGames() {
-  try {
-    const browser = await puppeteer.launch({ headless: "new" });
-    const page = await browser.newPage();
+	try {
+		const browser = await puppeteer.launch({
+			args: [
+				"--disable-setuid-sandbox",
+				"--no-sandbox",
+				"--single-process",
+				"--no-zygote",
+			],
+			executablePath:
+				process.env.NODE_ENV === "production"
+					? process.env.PUPPETEER_EXECUTABLE_PATH
+					: puppeteer.executablePath(),
+		});
+		const page = await browser.newPage();
 
-    await page.setExtraHTTPHeaders({
-      "Accept-Language": "en-US,en;q=0.9",
-    });
+		await page.setExtraHTTPHeaders({
+			"Accept-Language": "en-US,en;q=0.9",
+		});
 
-    await page.setUserAgent(
-      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
-    );
+		await page.setUserAgent(
+			"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+		);
 
-    await page.goto(BASE_URL);
+		await page.goto(BASE_URL);
 
-    const data = await page.evaluate(() => {
-      return document.body.innerHTML;
-    });
+		const data = await page.evaluate(() => {
+			return document.body.innerHTML;
+		});
 
-    const $ = cheerio.load(data);
-    const links = $(".user_game_list a")
-      .map((index, element) => {
-        const href = element.attribs.href;
-        return href ? href.split("/")[2] : null;
-      })
-      .get()
-      .filter(Boolean);
+		const $ = cheerio.load(data);
+		const links = $(".user_game_list a")
+			.map((index, element) => {
+				const href = element.attribs.href;
+				return href ? href.split("/")[2] : null;
+			})
+			.get()
+			.filter(Boolean);
 
-    let topGames = Array(25);
+		let topGames = Array(25);
 
-    const promises = links.map(async (index, i) => {
-      try {
-        const result = await hltbService.detail(index);
-        topGames[i] = result;
-      } catch (e) {
-        console.error(e);
-      }
-    });
+		const promises = links.map(async (index, i) => {
+			try {
+				const result = await hltbService.detail(index);
+				topGames[i] = result;
+			} catch (e) {
+				console.error(e);
+			}
+		});
 
-    let currentDate = new Date(Date.now());
-    currentDate.toISOString;
-    topGames.push(currentDate);
+		let currentDate = new Date(Date.now());
+		currentDate.toISOString;
+		topGames.push(currentDate);
 
-    await Promise.all(promises);
+		await Promise.all(promises);
 
-    await browser.close();
+		await browser.close();
 
-    let jsonData = JSON.stringify(topGames);
-    fs.writeFile("./public/topGames.json", jsonData, "utf-8");
-  } catch (error) {
-    console.log("Error:", error.message);
-    throw error;
-  }
+		let jsonData = JSON.stringify(topGames);
+		fs.writeFile("./public/topGames.json", jsonData, "utf-8");
+	} catch (error) {
+		console.log("Error:", error.message);
+		throw error;
+	}
 }
 
 module.exports = getTopGames;
