0b1876b7b73efe1dd53889b3757afaf774cfac33
Emilio Blacksmith <emilioblacksmithlush@gmail.com>
2023-12-31 18:59:34 -0600

    fix timeout limit puppeteer

--- modules/getTopGames.js
+++ modules/getTopGames.js
@@ -34,7 +34,7 @@ async function getTopGames() {
 			"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
 		);
 
-		await page.goto(BASE_URL);
+		await page.goto(BASE_URL, { waitUntil: "networkidle2", timeout: 0 });
 
 		const data = await page.evaluate(() => {
 			return document.body.innerHTML;
